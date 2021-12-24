9061149aa6667718199045cb4fa213285f29f352
Kai Han <kai.han@nio.com>
2021-12-24 17:22:22 +0800

    compare file size of executables that link to static and shared libraries.

--- static-and-dynamic-libs/math2.cpp
+++ static-and-dynamic-libs/math2.cpp
@@ -4,3 +4,19 @@ int add(int a, int b)
 {
     return a + b;
 }
+
+
+int minu(int a, int b) 
+{
+    return a - b;
+}
+
+int time(int a, int b)
+{
+    return a * b;
+}
+
+int divide(int a, int b) 
+{
+    return a / b;
+}
--- static-and-dynamic-libs/math2.h
+++ static-and-dynamic-libs/math2.h
@@ -2,5 +2,8 @@
 #define __MATH2_H__
 
 int add(int a, int b);
+int minu(int a, int b);
+int time(int a, int b);
+int divide(int a, int b);
 
 #endif // __MATH2_H__
