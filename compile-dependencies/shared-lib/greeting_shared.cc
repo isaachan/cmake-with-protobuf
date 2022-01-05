ba0f6028fcde50db1afd08d6a341dcce1ca33aec
Kai Han <kai.han@nio.com>
2022-01-05 17:30:53 +0800

    lib implementation.

---
+++ compile-dependencies/shared-lib/greeting_shared.cc
@@ -0,0 +1,11 @@
+#ifndef __GREETING_SHARED__
+#define __GREETING_SHARED__
+
+#include <iostream>
+
+void greeting_from_shared_lib()
+{
+    std::cout << "Greeting From Shared Lib" << std::endl;
+}
+
+#endif // __GREETING_SHARED__
---
+++ compile-dependencies/static-lib/greeting_static.cc
@@ -0,0 +1,11 @@
+#ifndef __GREETING_STATIC__
+#define __GREETING_STATIC__
+
+#include <iostream>
+
+void greeting_from_static_lib()
+{
+    std::cout << "Greeting From Static Lib" << std::endl;
+}
+
+#endif // __GREETING_STATIC__
